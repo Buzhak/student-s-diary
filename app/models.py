27e45193557bfcc163d7e2e966947f94a047a94a
Sergey Buzhak <byzhak@ya.ru>
2021-08-27 22:47:09 +0300

    added alembic

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from posixpath import realpath
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
@@ -6,31 +7,26 @@ from app.db import Base, engine
 
 
 class School_class(Base):
-    __tablename__ = 'school_class'
+    __tablename__ = 'school_classes'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(3))
     students = relationship('Student', back_populates='school_class')
 
+    def __repr__(self):
+        return f'Class name: {self.name}'
+
 
 class Student(Base):
     __tablename__ = 'student'
 
-    id = Column(Integer, primaty_key=True)
-    name = Column(String(100))
-    school_class_id = Column(Integer, ForeignKey('school_class.id'))
-    school_class = relationship('School_class', back_populates='students')
-    assessments = relationship('Assessment')
-
-
-class Assessment(Base):
-    __tablename__ = 'assessment'
-
     id = Column(Integer, primary_key=True)
-    assessment = Column(Integer(1))
-    student_id = Column(Integer, ForeignKey('student.id'))
-    school_subject_id = Column(Integer, ForeignKey('school_subject.id'))
-    school_subject = relationship('School_subject', back_populates='assessments')
+    name = Column(String(100))
+    school_class_id = Column(Integer, ForeignKey('school_classes.id'))
+    school_class = relationship('School_class', back_populates='students')
+
+    def __repr__(self):
+        return f'Student name: {self.name}'
 
 
 class School_subject(Base):
@@ -38,7 +34,19 @@ class School_subject(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(20))
-    assessments = relationship('Assessment', back_populates='school_subject')
+    
+    def __repr__(self):
+        return f'School_subjec: {self.name}'
+
+
+class Assessment(Base):
+    __tablename__ = 'assessment'
+
+    id = Column(Integer, primary_key=True)
+    assessment = Column(Integer)
+   
+    def __repr__(self):
+        return self.assessment
 
 if __name__ == ('__main__'):
     Base.metadata.create_all(bind=engine)
